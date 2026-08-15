bfdf07a26138c6ef567e9409f00568ca253bc24f
Lavinia <marialavinia801@yahoo.com>
2026-08-15 20:22:05 +0100

    new commit

--- api/contact.js
+++ api/contact.js
@@ -1,5 +1,12 @@
 import nodemailer from "nodemailer";
 
+export const config = {
+  api: {
+    bodyParser: true,
+  },
+};
+
+
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
